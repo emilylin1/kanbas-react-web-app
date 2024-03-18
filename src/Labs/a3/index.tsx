d18e3cdd4004644ff0e8e05ac81472c4ba231d94
emilylin1 <lin.emi@northeastern.edu>
2024-03-18 13:31:54 -0400

    lab 4

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -8,13 +8,23 @@ import Highlight from "./Highlight";
 import Add from "./Add";
 import TodoItem from "./todos/TodoItem";
 import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
+import { LabState } from "../store";
 
 function Assignment3() {
+    const { todos } = useSelector((state: LabState) => state.todosReducer);
     return (
         <div className="container">
             <h1>Assignment 3</h1>
-            <TodoList/>
-            <TodoItem/>
+            <ul className="list-group">
+                {todos.map((todo) => (
+                    <li className="list-group-item" key={todo.id}>
+                        {todo.title}
+                    </li>
+                ))}
+            </ul>
+            <TodoList />
+            <TodoItem />
             <Add a={3} b={4} />
             <Highlight>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
